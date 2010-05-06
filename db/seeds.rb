e412f54f344eb63a5dbcee9a773303786047dd39
Michael Staib <michael.staib@infinitelyeven.net>
2010-05-06 15:43:17 -0400

    Adds default administrative user to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+admin = User.create(:username => 'admin', :first => 'Default', :last => 'Admin', :is_admin => true, :password => 'admin', :password_confirmation => 'admin', :activated => true)
+admin.save_with_validation(false)
