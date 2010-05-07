05f56ba7681c093d58ecef852ac90126fe88e34d
mikldt <ditorm@rpi.edu>
2010-05-06 22:30:42 -0400

    Fix a whole slew of bugs related to project. Closes #26. Closes #22.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,5 @@
 class Project < ActiveRecord::Base
 	validates :name, :presence => true, :length => { :minimum => 3 } # A project must have a name.
-	validates :active, :presence => true # A project must be either active or inactive.
 	validates :group_id, :presence => true # A project must belong to a group.
 	belongs_to :group # Connects the project to the group that owns it.
 	has_many :tasks, :dependent => :destroy # Each project can have an unlimited number of tasks.
