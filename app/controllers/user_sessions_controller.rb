1d0cd2990f3de7b1697074032dbfe9e257584dc9
Michael Staib <michael.staib@infinitelyeven.net>
2010-05-07 01:26:04 -0400

    Removes redundant actions on user_sessions controller, closes #17

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,25 +1,5 @@
 class UserSessionsController < ApplicationController
-  # GET /user_sessions
-  # GET /user_sessions.xml
-  # Retrieves a list of all logged-in user sessions to be displayed.
-  def index
-    @user_sessions = UserSession.new
-  end
-
-  # GET /user_sessions/1
-  # GET /user_sessions/1.xml
-  # Displays the given logged-in user session.
-  def show
-    @user_session = UserSession.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @user_session }
-    end
-  end
-
-  # GET /user_sessions/new
-  # GET /user_sessions/new.xml
+  # GET /user_session/new
   # Offers a form to create a new user session, i.e. log in.
   def new
     @user_session = UserSession.new
@@ -30,8 +10,7 @@ class UserSessionsController < ApplicationController
     end
   end
 
-  # POST /user_sessions
-  # POST /user_sessions.xml
+  # POST /user_session
   # Creates a new user session, i.e. logs in.
   def create
     @user_session = UserSession.new(params[:user_session])
@@ -43,8 +22,7 @@ class UserSessionsController < ApplicationController
     end
   end
 
-  # DELETE /user_sessions/1
-  # DELETE /user_sessions/1.xml
+  # DELETE /user_session
   # Destroys the current user session, i.e. logs out.
   def destroy
     current_user_session.destroy
